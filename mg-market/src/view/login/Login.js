5a99bd87cc2b4f4b7688476e25d704d7bf84d497
kkh.kim <kkh.kim@lge.com>
2021-11-22 17:58:35 +0900

    init

---
+++ mg-market/src/view/login/Login.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
+
+function Login() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+export default Login;
